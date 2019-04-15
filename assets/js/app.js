bbbe9e2f74fe46988b429013e75dc6665ece23bc
caam123 <imac@Charlie-Charlie.local>
2019-04-15 16:14:27 -0500

    config data base, init var

---
+++ assets/js/app.js
@@ -0,0 +1,35 @@
+
+  var config = {
+    apiKey: "AIzaSyAsXBY4nCuJH3jwsvdJ2XdPvNYLFfethdM",
+    authDomain: "traintime-3402e.firebaseapp.com",
+    databaseURL: "https://traintime-3402e.firebaseio.com",
+    projectId: "traintime-3402e",
+    storageBucket: "traintime-3402e.appspot.com",
+    messagingSenderId: "263910394437"
+  };
+  firebase.initializeApp(config);
+
+  var dataRef = firebase.database();
+
+var name = "";
+var destination = "";
+var fristTrain = "";
+var frequency = "";
+
+
+
+$("#submit").on("click", function(){
+    event.preventDefault();
+    name = $("#name").val().trim();
+    destination = $("#destination").val().trim();
+    fristTrain = $("#time").val();
+    frequency = $("#frequency").val();
+
+    console.log(name);
+    console.log(destination);
+    console.log(fristTrain);
+    console.log(frequency);
+
+
+
+});
